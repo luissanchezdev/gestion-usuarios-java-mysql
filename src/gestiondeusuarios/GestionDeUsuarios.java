824892a158f315e2b2c8932dcecad0ddbe5985a7
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-12-29 02:56:06 -0500

    feature: added main form

---
+++ src/gestiondeusuarios/GestionDeUsuarios.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestiondeusuarios;
+
+import gestiondeusuarios.formularios.Formulario;
+
+/**
+ *
+ * @author luissdev
+ */
+public class GestionDeUsuarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario formularioAgregarUsuarios = new Formulario();
+        formularioAgregarUsuarios.show();
+    }
+    
+}
